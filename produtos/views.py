6c3e82a112a90365bc3989c0c2023e1e9b2d115e
Tarcizo <tarcizolsj@gmail.com>
2023-02-08 20:27:04 -0300

    Add novo vendedor

--- produtos/views.py
+++ produtos/views.py
@@ -2,12 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 def listagem_produtos(request):
-    context = {
-        'produtos': [{'nome': 'Uva', 'preco': 12}, 
-                     {'nome': 'Melância', 'preco': 10},
-                     {'nome': 'Banana', 'preco': 15}
-                    ]
+    produtos_dos_vendedores = [{
+        'vendedor': {'nome': 'Joseph Climber'},
+        'produtos': [
+            {'nome': 'Uva', 'preco': 12}, 
+            {'nome': 'Melância', 'preco': 10},
+            {'nome': 'Banana', 'preco': 15},
+        ]
+    },
+    {
+       'vendedor': {'nome': 'John Pay Pay'},
+       'produtos': [
+            {'nome': 'Carro', 'preco': 45000}, 
+            {'nome': 'Moto', 'preco': 1000},
+            {'nome': 'Bike', 'preco': 1500},
+        ]
     }
+    ]
+    context = {'produtos_dos_vendedores': produtos_dos_vendedores}
     return render(request, './templates/listagem_produtos.html', context)
 
 
