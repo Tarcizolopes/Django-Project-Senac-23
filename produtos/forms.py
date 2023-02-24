3736016a6c9f0bd68e0d376e056ccb7744ed9dbc
Tarcizo <tarcizolsj@gmail.com>
2023-02-24 19:40:43 -0300

    add imagem ao form

--- produtos/forms.py
+++ produtos/forms.py
@@ -4,9 +4,9 @@ from produtos.models import Produto
 class ProdutoModelForm(ModelForm):
     class Meta:
         model = Produto
-        fields = ['nome', 'preco', 'estoque', 'anunciante']
+        fields = ['nome', 'preco', 'estoque', 'anunciante', 'imagem']
 
 class ServicoModelForm(ModelForm):
     class Meta:
         model = Produto
-        fields = ['nome', 'preco', 'anunciante']
+        fields = ['nome', 'preco', 'anunciante', 'imagem']
